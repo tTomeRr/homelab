8399cac858686001973803ca0ec2f030ed96450e
Tomer Cohen <tomerc248@gmail.com>
2026-01-10 19:40:57 +0200

    add terraform provision for VMs in proxmox

--- proxmox/terraform/main.tf
+++ proxmox/terraform/main.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url      = var.proxmox_api_url
+  pm_api_url          = var.proxmox_api_url
   pm_api_token_id     = var.proxmox_token_id
   pm_api_token_secret = var.proxmox_token_secret
-  pm_tls_insecure = true
+  pm_tls_insecure     = true
 }
 
 resource "proxmox_vm_qemu" "k3s_master" {
